b530702854c1fc592e2f2346f43f53578a71f017
Aishat <aishatomolara2020@gmail.com>
2025-06-15 04:11:01 +0100

    updated now

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URL)
     });
 
     app.get("/", (req,res)=>{
-        res.status(200).json({message: "Welcome to  My CareerEx API"});
+        res.status(200).json({message: "Welcome to  My CareerEx Final Project API"});
     })
 
 app.use(routes);
